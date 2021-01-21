3c5e75a3abaeb1f252b49dfa21ac75da552a5a49
QW999 <66690045+QW999@users.noreply.github.com>
2021-01-21 13:01:55 +0200

    Update list_5.py

--- VANAR/list_5.py
+++ VANAR/list_5.py
@@ -4,55 +4,60 @@ from os import system
 
 option = ''
 over = False
-robo_x = 5 # index 4
-gmap = ['D','','','','x','♖','M','','','']  # 10values / index 9
+robo_x = 5 # index 5
+gmap = [' ',' ',' ',' ',' ','♖',' ',' ','⚔',' ']  # 10values / index 9
 
 while option != 'x':
+    try:
 
-    ########## print the map #############
-    system("cls")
-    print()
-    print("-" * 20)
-    for i in range(len(gmap)):
-        print(gmap[i], end=' ')
-    print()
-    print("-" * 20)
-    for i in range(len(gmap)):
-        print(i, end=' ')
-    print('\n\nCONTROLS: a - left, d - right, x - exit')
-    ########## print the map #############
+        ########## print the map #############
+        system('cls')
+        print()
+        print("-" * 20)
+        for i in range(len(gmap)):
+            print(gmap[i], end=' ')
+        print()
+        print("-" * 20)
+        for i in range(len(gmap)):
+            print(i, end=' ')
+        print('\n\nCONTROLS: a - left, d - right, x - exit')
+        ########## print the map #############
 
 
-    ########## controls #############
+        ########## controls #############
 
-    if over:
-        print('GAME OVER')
-        break
-    option = input('>> ')
-    if option == 'a':
-        # print('left')
-        gmap[robo_x] = ' '  # remove thr current position
-        robo_x -= 1
-        if gmap[robo_x] == 'D':  # Dinamit
-            gmap[robo_x] = 'M'  # Mort
-            over = True
-        else:
-            gmap[robo_x] = 'x'
+        if over:
+            print('GAME OVER')
+            break
 
-    elif option == 'd':
-        # print('right')
-        gmap[robo_x] = ' '  # remove thr current position
-        robo_x += 1
-        # gmap[robo_x] = 'x'
-        if gmap[robo_x] == 'D':  # Dinamit
-            gmap[robo_x] = 'M'  # Mort
-        else:
-            gmap[robo_x] = 'x'
+        option = input('>> ')
+        if option == 'a':
+            # print('left')
+            gmap[robo_x] = ' '  # remove thr current position
+            robo_x -= 1
+            if gmap[robo_x] == '⚔':  # Dinamit
+                gmap[robo_x] = '☠'  # Mort
+                over = True
+            else:
+                gmap[robo_x] = '♖'   # placeit on this position
 
-    elif option == 'x':
-        print('GAME OVER')
-    ########## controls #############
+        elif option == 'd':
+            # print('right')
+            gmap[robo_x] = ' '
+            robo_x += 1
+            if gmap[robo_x] == '⚔':  # Dinamit
+                gmap[robo_x] = '☠'  # Mort
+                over = True
+            else:
+                gmap[robo_x] = '♖'
 
+
+        elif option == 'x':
+            print('GAME OVER')
+        ########## controls #############
+
+    except IndexError:
+        print('list assignment index out of range')
 
 
 
