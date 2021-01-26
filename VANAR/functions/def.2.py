f2296ed96b87b7c5bce3952cbba0a69f986648db
QW999 <66690045+QW999@users.noreply.github.com>
2021-01-26 22:27:36 +0200

    Update def.2.py

--- VANAR/functions/def.2.py
+++ VANAR/functions/def.2.py
@@ -1,20 +1,20 @@
 
 
-# def Week_eval_temperature(Lu, Ma, Mi, Jo, Vi, Sa, Du):
 def Week_eval_temperature():
-    # Week_temperature = (Lu + Ma + Mi + Jo + Vi + Sa + Du) / 7
-    # temp = int(input('Today temperature: '))
     Lu = int(input('Lu t: '))
     Ma =  int(input('Ma t: '))
     Mi =  int(input('Mi t: '))
     Jo =  int(input('Jo t: '))
     Vi =  int(input('Vi t: '))
     Sa =  int(input('Sa t: '))
-    Du =  int(input('Du t: ')))
+    Du =  int(input('Du t: '))
 
     temp = (Lu + Ma + Mi + Jo + Vi + Sa + Du) / 7
+    print('Week_eval_temperature: ', round(temp, 2), '°C')
 
-    # print('Week_eval_temperature: ', round(Week_temperature, 2))
+    ct = input('Enter city: ')
+
+
     def eval_temperature(temp):
         if temp >= 50:
             print("Very hot")
@@ -30,7 +30,23 @@ def Week_eval_temperature():
             print("Very Cold")
 
     # eval_temperature(int(input('Today temperature: ')))
+    print('eval_temperature: ')
     eval_temperature(temp)
+    et = eval_temperature(temp)
 
 
+    def city(ct):
+        RM = ['Anenii Noi','Basarabeasca','Briceni','Cahul','Cantemir','Călărași','Căușeni','Cimișlia',
+              'Criuleni','Dondușeni','Drochia','Dubăsari','Edineț','Fălești','Florești','Glodeni',
+              'Hîncești','Ialoveni','Leova','Nisporeni','Ocnița','Orhei','Rezina','Rîșcani','Sîngerei',
+              'Soroca','Strășeni','Șoldănești','Ștefan Vodă','Taraclia','Telenești','Ungheni']
+        if ct in RM:
+            print('In RM, ', ct, ' weather is', round(temp, 2), '°C'", 'is', eval_temperature(temp)")
+        else:
+            print('Over the border in ', ct, ' weather is', round(temp, 2), '°C'", 'is', eval_temperature(temp)")
+    city(ct)
+
+
+
+print('Week_eval_temperature')
 Week_eval_temperature()
