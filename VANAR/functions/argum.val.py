6624846c3f5cea8145d912d5e0ec313aff840d93
QW999 <66690045+QW999@users.noreply.github.com>
2021-02-01 10:30:22 +0200

    Add files via upload

---
+++ VANAR/functions/argum.val.py
@@ -0,0 +1,44 @@
+
+# string = "3.141"
+# print(string)
+# print(type(string))
+#
+# # converting string to float
+# Float = float(string)
+# print(Float)
+# print(type(Float))
+
+
+def inputInt(message_i):
+    print('message_i', type(message_i))
+    int_mes = int(message_i)
+    print('int_mes: ', int_mes)
+    print('int_mes', type(int_mes))
+    return int_mes
+inputInt(input('Enter inputInt message: '))
+
+n = inputInt(input("Enter the first integer: "))
+m = inputInt(input("Enter the second integer: "))
+
+print( n + m )
+
+
+
+def inputFloat(message_f):
+    print('message_f', type(message_f))
+    float_mes = float(message_f)
+    print('float_mes: ', float_mes)
+    print('float_mes', type(float_mes))
+    return float_mes
+inputFloat(input('Enter inputFloat message: '))
+
+
+
+
+def inputBoolean(message_b):
+    print('message_b', type(message_b))
+    bool_mes = bool(message_b)
+    print('bool_mes: ', bool_mes)
+    print('bool_mes', type(bool_mes))
+    return bool_mes
+inputBoolean(input('Enter inputBoolean message: '))
