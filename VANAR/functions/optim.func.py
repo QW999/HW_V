13e73612d94f57d27663447153b0bde7acd58ceb
QW999 <66690045+QW999@users.noreply.github.com>
2021-02-11 10:06:34 +0200

    Delete VANAR directory

--- VANAR/functions/optim.func.py
+++
@@ -1,9 +0,0 @@
-
-def drawLine( length, direction ):
-    if direction == 'h':
-        print('-' * length)
-    if direction == 'v':
-        print('\n|' * length)
-
-drawLine( 5, 'h' )
-drawLine( 3, 'v' )
--- VANAR/w2.py
+++
@@ -1,7 +0,0 @@
-gmap = [' ',' ',' ',' ',' ','♖',' ',' ','⚔',' ']
-for i in range(len(gmap)):
-    print(i, end=' ')
-    if i <= 0:
-        break
-    if i >= 9:
-        break
