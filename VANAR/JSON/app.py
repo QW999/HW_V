3c90ca7c649e6f5b2f41cddaf14b73744523cad0
QW999 <66690045+QW999@users.noreply.github.com>
2021-02-13 04:35:24 +0200

    Add files via upload

---
+++ VANAR/JSON/app.py
@@ -0,0 +1,15 @@
+
+
+# str JSON --> structure --> python
+
+# json.load()  # reads from file
+# json.loads()  # reads from str
+
+
+import json
+
+file = open("data/person.json", "r")
+person = json.load(file)
+
+print(person)
+print(type(person))
