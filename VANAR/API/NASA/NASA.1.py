473c9099dbcc63bc7bc0abd7f6ee12bc16ff5fbc
QW999 <66690045+QW999@users.noreply.github.com>
2021-02-14 15:03:48 +0200

    Update NASA.1.py

--- VANAR/API/NASA/NASA.1.py
+++ VANAR/API/NASA/NASA.1.py
@@ -4,7 +4,6 @@ import pprint
 
 pp = pprint.PrettyPrinter(indent=2)
 
-
 NASA_APIs = 'https://api.nasa.gov/planetary/apod?api_key=eHv7Q0ZedKx7JwWVhYooeev0RAoLshLAWyUuKbm4'
 # APIs_key = 'eHv7Q0ZedKx7JwWVhYooeev0RAoLshLAWyUuKbm4'
 # NASA_url = 'https://api.nasa.gov/planetary/apod?api_key='
@@ -13,12 +12,8 @@ response = requests.get(NASA_APIs)
 data = json.loads(response.text)
 # pp.pprint(data)
 
-# print(data['title'])
-# print('current_date: ', data['date'])
-# print('url: ', data['url'])
+print(data['title'])
+print('current_date: ', data['date'])
+print('url: ', data['url'])
 
 
-Asteroids_Neo_Feed = 'https://api.nasa.gov/planetary/apod?api_key=vh4MyOD88FqMEgCpPK8dM1VVJqkpRf8h6wyAWplK'
-response_2 = requests.get(Asteroids_Neo_Feed)
-data_2 = json.loads(response_2.text)
-pp.pprint(data_2)
