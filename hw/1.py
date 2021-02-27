dbee4367befa46ce44bd868213e6746aea3b9507
QW999 <66690045+QW999@users.noreply.github.com>
2021-02-27 22:02:46 +0200

    Update 1.py

--- hw/1.py
+++ hw/1.py
@@ -25,3 +25,26 @@ Skoda = Car("Skoda Octavia", 13)
 print(Honda)
 print(Skoda)
 
+
+
+
+
+class Point:
+
+    def __init__(self, x = 0, y = 0):
+      self.x = x
+      self.y = y
+
+    def __sub__(self, other):
+        x = self.x + other.x
+        y = self.y + other.y
+        # print(str(self.x))
+        # print(str(self.y))
+        # print(str(other.x))
+        # print(str(other.y))
+        return Point(x, y)
+
+p1 = Point(3, 4)
+p2 = Point(1, 2)
+result = p1-p2
+print(result.x, result.y) # prints (4,6)
