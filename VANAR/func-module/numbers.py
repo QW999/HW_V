51d1f40bf12a7dad0ac185a2ac9bb55cc150c19e
QW999 <66690045+QW999@users.noreply.github.com>
2021-02-27 10:30:36 +0200

    Delete VANAR directory

--- VANAR/func-module/numbers.py
+++
@@ -1,6 +0,0 @@
-
-def pow_2(num):
-    return num * num
-
-def pow_3(num):
-    return pow_2(num) * num
--- VANAR/work/Positive.py
+++
@@ -1,6 +0,0 @@
-
-# Nr pozitive impare
-
-value = int(input())
-print("Positive") if value >= 0 else print("Negative")
-print("Odd") if value % 2 != 0 else print("Even")
